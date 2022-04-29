c16c76f7e4197e0961232ebca207cba3d4c201fc
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2022-04-28 22:29:20 -0500

    db

---
+++ portfolio/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+class project(models.Model):
+
+    title = CharField(max_length=100)
+    descripction = models.CharField(max_length=250)
+    image = models.ImageField( upload_to='portfolio/images/')
+    url=models.URLField(blank=True)
