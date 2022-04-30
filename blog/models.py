55fef683fcc9c2b23c0be97d169a7a4d78c45943
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2022-04-29 21:27:15 -0500

    navbar

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+from distutils import text_file
+from pydoc import describe
+from tkinter import Image
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    descripction = models.TextField()
+    image = models.ImageField(upload_to='blog/images')
+    date  = models.DateField(datetime.date.today)
